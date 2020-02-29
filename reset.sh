462b967795846b9f0746c84983b5be8ab4d73f7a
root <root@bastion.c.redislabs-sa-training-services.internal>
2020-02-29 20:48:58 +0000

    reset

--- reset.sh
+++ reset.sh
@@ -1,5 +1,12 @@
-ssh kube-master-0 kubeadm reset -f 
-ssh kube-master-1 kubeadm reset -f 
-ssh kube-master-2 kubeadm reset -f 
-ssh kube-worker-0 kubeadm reset -f 
-
+ssh kube-master-0 "kubeadm reset -f ;rm -rf $HOME/.kube/config ; rm -rf /etc/kubernetes;"
+ssh kube-master-1 "kubeadm reset -f ; rm -rf $HOME/.kube/config ; rm -rf /etc/kubernetes;"
+ssh kube-master-2 "kubeadm reset -f ; rm -rf $HOME/.kube/config ; rm -rf /etc/kubernetes;"
+ssh kube-worker-0 "kubeadm reset -f ; rm -rf $HOME/.kube/config ; rm -rf /etc/kubernetes;"
+ssh kube-worker-1 "kubeadm reset -f ; rm -rf $HOME/.kube/config ; rm -rf /etc/kubernetes;"
+ssh kube-worker-2 "kubeadm reset -f ; rm -rf $HOME/.kube/config ; rm -rf /etc/kubernetes;"
+ssh kube-storage-0 "kubeadm reset -f ; rm -rf $HOME/.kube/config ; rm -rf /etc/kubernetes;"
+ssh kube-storage-1 "kubeadm reset -f ; rm -rf $HOME/.kube/config ; rm -rf /etc/kubernetes;"
+ssh kube-storage-2 "kubeadm reset -f ; rm -rf $HOME/.kube/config ; rm -rf /etc/kubernetes;"
+ssh kube-ingress-0 "kubeadm reset -f ; rm -rf $HOME/.kube/config ; rm -rf /etc/kubernetes;"
+ssh kube-ingress-1 "kubeadm reset -f ; rm -rf $HOME/.kube/config ; rm -rf /etc/kubernetes;"
+ssh kube-ingress-2 "kubeadm reset -f ; rm -rf $HOME/.kube/config ; rm -rf /etc/kubernetes;"
